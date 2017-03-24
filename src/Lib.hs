16b43a680d34d3c1d5b0e345360bbc5498ef05ee
techno-tanoC <develop.tanoc@gmail.com>
2017-03-24 20:33:32 +0900

    make simple

--- src/Lib.hs
+++ src/Lib.hs
@@ -20,18 +20,22 @@ isval TmFalse = True
 isval t | isnumericval t = True
 isval _ = False
 
-eval1 :: Term ->  Maybe Term
-eval1 (TmIf TmTrue x _) = pure x
-eval1 (TmIf TmFalse _ y) = pure y
-eval1 (TmIf b x y) = TmIf <$> eval1 b <*> pure x <*> pure y
-eval1 (TmSucc x) = TmSucc <$> eval1 x
-eval1 (TmPred TmZero) = pure TmZero
-eval1 (TmPred (TmSucc x)) | isnumericval x = pure x
-eval1 (TmPred x) = TmPred <$> eval1 x
-eval1 (TmIsZero TmZero) = pure TmTrue
-eval1 (TmIsZero (TmSucc x)) | isnumericval x = pure TmFalse
-eval1 (TmIsZero x) = TmIsZero <$> eval1 x
-eval1 x = Nothing
+eval1 :: Term ->  Term
+eval1 (TmIf TmTrue x _) = x
+eval1 (TmIf TmFalse _ y) = y
+eval1 (TmIf b x y) = TmIf (eval1 b) x y
+eval1 (TmSucc x) = TmSucc $ eval1 x
+eval1 (TmPred TmZero) = TmZero
+eval1 (TmPred (TmSucc x)) | isnumericval x = x
+eval1 (TmPred x) = TmPred $ eval1 x
+eval1 (TmIsZero TmZero) = TmTrue
+eval1 (TmIsZero (TmSucc x)) | isnumericval x = TmFalse
+eval1 (TmIsZero x) = TmIsZero $ eval1 x
+eval1 x = x
 
 eval :: Term -> Term
-eval t = maybe t eval $ eval1 t
+eval t =
+  let t' = eval1 t
+  in if t == t'
+    then t'
+    else eval t'
